66445b20f07e6ec747a7c818283261f46a8c9619
CalebVincent <vincent_806@yahoo.com>
2020-03-05 02:25:14 -0600

    Added paths to Link elements on Home component

--- src/components/Home/Home.js
+++ src/components/Home/Home.js
@@ -1,19 +1,14 @@
 import React from "react";
-import "./style.css"
-
-
+import "./style.css";
+import { Link } from 'react-router-dom';
 
 function Home() {
     return (
-        <div>
-            <div>
-                <div id="hero-text">
-                    <h1>Caleb Vincent</h1>
-                    <p>And I'm a Web Developer</p>
-                    <button>Contact Me</button>
-                    <button>View Previous Work</button>
-                </div>
-            </div>
+        <div id="hero-text">
+            <h1>Caleb Vincent</h1>
+            <p>And I'm a Web Developer</p>
+            <Link className="btn btn-primary" to="/contact">Contact Me  </Link>|
+            <Link id="galleryBtn" to="/gallery">              View Previous Work</Link>
         </div>
     )
 }
--- src/components/Wrapper/Wrapper.js
+++ src/components/Wrapper/Wrapper.js
@@ -3,6 +3,7 @@ import "./style.css";
 
 function Wrapper(props) {
   return <main id= "hero-image" className="wrapper" {...props} />;
+  
 }
 
 export default Wrapper;
